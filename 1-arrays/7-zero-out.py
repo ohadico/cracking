9b0d38a5c660c4b235a7e8c5461310f1786ac310
Ohad Cohen <no-mail>
2020-02-26 22:40:50 +0200

    1.7 tested

--- 1-arrays/7-zero-out.py
+++ 1-arrays/7-zero-out.py
@@ -12,3 +12,17 @@ def zero_out(mat):
             if i in rows or j in cols:
                 mat[i][j] = 0
     return mat
+
+
+def test_zero_out():
+    mat = [[1, 2, 3, 4],
+           [0, 2, 3, 0],
+           [1, 2, 3, 4],
+           [0, 2, 3, 4]]
+    res = [[0, 2, 3, 0],
+           [0, 0, 0, 0],
+           [0, 2, 3, 0],
+           [0, 0, 0, 0]]
+    assert zero_out([]) == []
+    assert zero_out([[0]]) == [[0]]
+    assert zero_out(mat) == res
