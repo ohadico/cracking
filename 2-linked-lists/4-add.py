a860b4fd9f5493123890b2920aedde45277b8f93
Ohad Cohen <no-mail>
2020-02-27 01:25:33 +0200

    2.4 tested

--- 2-linked-lists/4-add.py
+++ 2-linked-lists/4-add.py
@@ -1,6 +1,6 @@
 from itertools import zip_longest
 
-from linkedlist import Node
+from linkedlist import Node, linkedlist
 
 
 def add(linked1, linked2):
@@ -9,7 +9,15 @@ def add(linked1, linked2):
     for a, b in zip_longest(linked1, linked2, fillvalue=0):
         c += a + b
         last.next = Node(c % 10)
-        c /= 10
+        last = last.next
+        c //= 10
     if c:
         last.next = Node(c)
     return head.next
+
+
+def test_add():
+    a = linkedlist((3, 1, 5))
+    b = linkedlist((5, 9, 2))
+    c = add(a, b)
+    assert list(c) == [8, 0, 8]
