06880e6e100bb7b7fadb113e3e99bf11291f9da5
Ohad Cohen <no-mail>
2020-02-27 00:52:28 +0200

    2.2 tested

--- 2-linked-lists/2-nth.py
+++ 2-linked-lists/2-nth.py
@@ -1,6 +1,22 @@
+import pytest
+
+from linkedlist import linkedlist
+
+
 def nth(linked, n):
     buffer = [None] * n
     for i in linked:
         buffer.pop(0)
         buffer.append(i)
     return buffer.pop(0)
+
+
+@pytest.mark.parametrize(("end", 'n'),
+                         [(10, 1),
+                          (10, 3),
+                          (10, 10),
+                          (10, 12)])
+def test_nth(end, n):
+    seq = range(1, end + 1)
+    head = linkedlist(seq)
+    assert nth(head, n) == (seq[-n] if n <= end else None)
