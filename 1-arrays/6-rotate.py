546a40a3cc5ea60a6819e9b214102ad06ffcb623
Ohad Cohen <no-mail>
2020-02-26 22:23:17 +0200

    1.6 tested

--- 1-arrays/6-rotate.py
+++ 1-arrays/6-rotate.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def rotate90(img):
     n = len(img)
     for i in range(n - 1):
@@ -9,3 +12,8 @@ def rotate90(img):
             img[i][j], img[i][n - j - 1] = img[i][n - j - 1], img[i][j]
 
     return img
+
+
+def test_rotate90():
+    img = np.arange(1, 10).reshape((3, 3))
+    assert np.all(rotate90(np.copy(img)) == np.rot90(img, 3))
