9e26711890a67dd3901bafd865f6facea7fe00c9
Ohad Cohen <no-mail>
2020-02-26 22:45:11 +0200

    1.8 fixed

--- 1-arrays/8-is-rotation.py
+++ 1-arrays/8-is-rotation.py
@@ -1,2 +1,7 @@
 def is_rotation(str1, str2):
-    return str2 in str1*2
+    return len(str1) == len(str2) and str2 in str1*2
+
+
+def test_is_rotation():
+    assert is_rotation("waterbottle", "erbottlewat")
+    assert is_rotation("waterbottle", "rbottlewat") is False
