3d88a8f2eae26f5209c4f57e03de78bd6905bb46
Ohad Cohen <no-mail>
2020-02-27 00:37:06 +0200

    2.1 fixed

--- 2-linked-lists/1-remove-dups.py
+++ 2-linked-lists/1-remove-dups.py
@@ -1,15 +1,28 @@
+from linkedlist import linkedlist
+
+
 def remove_dups(node):
     if node is None:
         return
-    head = tail = node
+    head = prev = node
     curr = node.next
     while curr is not None:
         temp = head
-        while temp != curr and temp.val != curr.val:
+        while temp.val != curr.val:
             temp = temp.next
-        if temp.val != curr.val:
-            tail.next = curr
-            tail = tail.next
-        curr = curr.next
-    tail.next = None
+        if temp != curr:
+            curr = curr.next
+        else:
+            prev.next = curr
+            curr = curr.next
+            prev = prev.next
+    prev.next = None
     return head
+
+
+def test_remove_dups():
+    assert list(remove_dups(linkedlist([1]))) == [1]
+    assert list(remove_dups(linkedlist([1, 1]))) == [1]
+    assert list(remove_dups(linkedlist(range(5)))) == list(range(5))
+    assert list(remove_dups(linkedlist([1, 2, 1, 1, 1, 1]))) == [1, 2]
+    assert list(remove_dups(linkedlist([1, 2, 3, 2, 1, 3, 2, 1, 2, 2]))) == [1, 2, 3]
