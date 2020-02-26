8f550978a61373ea764a860e762868c835de5cfc
Ohad Cohen <no-mail>
2020-02-26 21:31:00 +0200

    1.4

---
+++ 1-arrays/4-anagrams.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+
+
+# space: O(n), time: O(n)
+def is_anagrams(str1, str2):
+    if len(str1) != len(str2):
+        return False
+    counter = defaultdict(int)
+    for c1, c2 in zip(str1, str2):
+        counter[c1] += 1
+        counter[c2] -= 1
+    return not any(counter)
