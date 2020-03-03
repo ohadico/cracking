15c2e16825a9013c560ba8b3f183db6d9a6ac510
Ohad Cohen <ohadbyte@gmail.com>
2020-03-03 22:05:35 +0200

    3.2 improved

--- 3-stacks/2-min-stack.py
+++ 3-stacks/2-min-stack.py
@@ -1,28 +1,42 @@
-import math
+from typing import Tuple, List
 
 
-def push(stack, new_val):
-    stack.append((new_val, get_min(stack)))
+def push(stack: Tuple[List, List[Tuple]], new_val):
+    stack[0].append(new_val)
+    if stack[1]:
+        last_min = stack[1][-1]
+        if new_val < last_min[0]:
+            stack[1].append((new_val, 1))
+        elif new_val == last_min[0]:
+            stack[1][-1] = (last_min[0], last_min[1]+1)
+    else:
+        stack[1].append((new_val, 1))
 
 
 def pop(stack):
-    return stack.pop()[0]
+    val = stack[0].pop()
+    last_min = stack[1][-1]
+    if val == last_min[0]:
+        if last_min[1] == 1:
+            stack[1].pop()
+        else:
+            stack[1][-1] = (last_min[0], last_min[1]-1)
+    return val
 
 
 def get_min(stack):
-    if stack:
-        return min(stack[-1])
-    return math.inf
+    if stack[1]:
+        return stack[1][-1][0]
 
 
 def test_min_stack():
-    stack = []
+    stack = ([], [])
     for i in (3, 1, 2, 0):
         push(stack, i)
 
     assert get_min(stack) == 0
-    stack.pop()
+    pop(stack)
     assert get_min(stack) == 1
-    stack.pop()
-    stack.pop()
-    stack.pop()
+    pop(stack)
+    pop(stack)
+    pop(stack)
